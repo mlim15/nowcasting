168a4b2c9d901939898228a004bc8352da9befad
mlim15 <27998345+the-salami@users.noreply.github.com>
2020-06-10 03:02:27 -0400

    introduce timeout for dio requests, and halt update process if fetching headers fail, this prevents lockup under poor network conditions

--- lib/support-update.dart
+++ lib/support-update.dart
@@ -6,16 +6,31 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:intl/intl.dart';
 import 'package:image/image.dart' as imglib;
-import 'package:tesseract_ocr/tesseract_ocr.dart';
 
 import 'package:Nowcasting/support-ux.dart' as ux;
 import 'package:Nowcasting/support-io.dart' as io;
 import 'package:Nowcasting/support-imagery.dart' as imagery;
 
-var dio = Dio();
+var dio = Dio(BaseOptions(connectTimeout: 2000));
 String headerFormat = "EEE, dd MMM yyyy HH:mm:ss zzz";
 
 imglib.PngDecoder pngDecoder = new imglib.PngDecoder();
+
+// Manipulating DateTime
+extension on DateTime{
+  DateTime roundDown([Duration delta = const Duration(minutes: 10)]) {
+    return DateTime.fromMillisecondsSinceEpoch(
+        this.millisecondsSinceEpoch -
+        this.millisecondsSinceEpoch % delta.inMilliseconds
+    );
+  }
+  DateTime roundUp([Duration delta = const Duration(minutes: 10)]) {
+    return DateTime.fromMillisecondsSinceEpoch(
+        this.millisecondsSinceEpoch + delta.inMilliseconds - 
+        this.millisecondsSinceEpoch % delta.inMilliseconds
+    );
+  }
+}
 
 // Functions for checking availability and updating files from remote source
 Future<bool> checkUpdateAvailable(String url, File file) async {
@@ -26,8 +41,11 @@ Future<bool> checkUpdateAvailable(String url, File file) async {
     urlLastModHeader = header.headers.value(HttpHeaders.lastModifiedHeader);
   } catch(e) {
     print('update.checkUpdateAvailable: Failed to get header for $url');
-    // return true by default on failure
-    return true;
+    // return false by default on failure
+    // otherwise in bad network conditions, refreshing takes forever
+    // and the status becomes absolutely unclear to the user.
+    // this is especially serious for the splash screen.
+    throw('Error: could not get header for $url, cancelling');
   }
   DateTime urlLastModified = DateFormat(headerFormat).parse(urlLastModHeader, true);
   bool updateAvailable = fileLastModified.isBefore(urlLastModified);
@@ -82,23 +100,19 @@ remoteImagery(BuildContext context, bool forceRefresh, bool notSilent) async {
   // Download all the images using our downloadFile method.
   // The HTTP last modified header is individually checked for each file before downloading.
   for (int i = 0; i <= 8; i++) {
-    if (forceRefresh || await checkUpdateAvailable('https://radar.mcgill.ca/dynamic_content/nowcasting/forecast.$i.png', io.localFile('forecast.$i.png'))) {
-      if (notYetShownStartSnack) {ux.showSnackBarIf(notSilent, ux.refreshingSnack, context, 'update.remoteImagery: An image was found that needs updating, starting update');notYetShownStartSnack=false;}
-      try {
-        downloadFile('https://radar.mcgill.ca/dynamic_content/nowcasting/forecast.$i.png', io.localFilePath('forecast.$i.png'));
-      } catch(e) {
-        ux.showSnackBarIf(notSilent, ux.errorRefreshSnack, context, 'update.remoteImagery: Error updating image number $i, stopping');
-        return false;
+    try {
+      if (forceRefresh || await checkUpdateAvailable('https://radar.mcgill.ca/dynamic_content/nowcasting/forecast.$i.png', io.localFile('forecast.$i.png'))) {
+        if (notYetShownStartSnack) {ux.showSnackBarIf(notSilent, ux.refreshingSnack, context, 'update.remoteImagery: An image was found that needs updating, starting update');notYetShownStartSnack=false;}
+        try {
+          await downloadFile('https://radar.mcgill.ca/dynamic_content/nowcasting/forecast.$i.png', io.localFilePath('forecast.$i.png'));
+        } catch(e) {
+          ux.showSnackBarIf(notSilent, ux.errorRefreshSnack, context, 'update.remoteImagery: Error updating image number $i, stopping');
+          return false;
+        }
       }
-    }
-    if (forceRefresh || await checkUpdateAvailable('https://radar.mcgill.ca/dynamic_content/nowcasting/forecast_legend.$i.png', io.localFile('forecast_legend.$i.png'))) {
-      if (notYetShownStartSnack) {ux.showSnackBarIf(notSilent, ux.refreshingSnack, context, 'update.remoteImagery: An image was found that needs updating, starting update');notYetShownStartSnack=false;}
-      try {
-        downloadFile('https://radar.mcgill.ca/dynamic_content/nowcasting/forecast_legend.$i.png', io.localFilePath('forecast_legend.$i.png'));
-      } catch(e) {
-        ux.showSnackBarIf(notSilent, ux.errorRefreshSnack, context, 'update.remoteImagery: Error updating image legend $i, stopping');
-        return false;
-      }
+    } catch(e) {
+      ux.showSnackBarIf(notSilent, ux.errorRefreshSnack, context, 'update.remoteImagery: Error updating image number $i, stopping');
+      return false;
     }
   }
   if (notYetShownStartSnack) {
@@ -108,9 +122,6 @@ remoteImagery(BuildContext context, bool forceRefresh, bool notSilent) async {
     ux.showSnackBarIf(notSilent, ux.noRefreshSnack, context, 'update.remoteImagery: No images needed updating');
     return false;
   } else {
-    // Clear the image cache.
-    await forecasts();
-    await legends();
     // Show a notification saying we successfully refreshed.
     ux.showSnackBarIf(notSilent, ux.refreshedSnack, context, 'update.remoteImagery: Image update successful');
     return true;
@@ -119,6 +130,7 @@ remoteImagery(BuildContext context, bool forceRefresh, bool notSilent) async {
 
 // Local product updates
 forecasts() async {
+  print('update.forecasts: Starting update process');
   // Clear the array and reload all the images into it
   // Note that the array is a class-level variable in forecast.dart
   try {
@@ -127,22 +139,25 @@ forecasts() async {
     }
   } catch(e) {
     print(e);
-    print("update.forecasts: Could not clear image array.");
+    print("update.forecasts: Could not clear image array");
   }
   for (int i = 0; i <= 8; i++) {
     if (await io.localFile('forecast.$i.png').exists()) {
+      print('update.forecasts: decoding image $i of 8.');
       imagery.forecasts.add(pngDecoder.decodeImage(await io.localFile('forecast.$i.png').readAsBytes()));
     }
   }
+  print('update.forecasts: Done decoding images');
 }
 
 legends() async {
+  print('update.legends: Starting update process');
   if (imagery.legends.isNotEmpty) {
     imagery.legends.clear();
   }
   for (int i = 0; i <= 8; i++) {
-    if (await io.localFile('forecast_legend.$i.png').exists()) {
-      imagery.legends.add(await TesseractOcr.extractText(io.localFilePath('forecast_legend.$i.png'), language: 'eng'));
+    if (await io.localFile('forecast.$i.png').exists()) {
+      imagery.legends.add((await io.localFile('forecast.$i.png').lastModified()).toUtc().roundUp(Duration(minutes: 10)).add(Duration(minutes: 20*i)).toString());
     }
   }
   // For debug purposes
