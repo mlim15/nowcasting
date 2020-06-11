d61618686301b18d9b0604b99e45f73610626d8e
mlim15 <27998345+the-salami@users.noreply.github.com>
2020-06-10 23:03:09 -0400

    basic forecast screen

--- lib/UI-forecast.dart
+++ lib/UI-forecast.dart
@@ -4,6 +4,8 @@ import 'package:latlong/latlong.dart';
 import 'package:Nowcasting/support-update.dart' as update;
 import 'package:Nowcasting/support-imagery.dart' as imagery;
 
+LatLng testCoord = ***REMOVED***;
+
 // Widgets
 class ForecastScreen extends StatefulWidget  {
   @override
@@ -29,8 +31,16 @@ class ForecastScreenState extends State<ForecastScreen> {
               alignment: Alignment(0,0), 
               child: Column(
                 children: [
-                  Icon(Icons.warning), 
-                  Text("Under Construction")
+                  Row(children: <Widget>[Text(testCoord.toString())],),
+                  Row(children: <Widget>[Text(imagery.legends[0]), Container(child: Icon(Icons.info, color: Color(0x00000000)), color: imagery.dec2hex(imagery.getPixelValue(imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[0], imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[1], 0)))]),
+                  Row(children: <Widget>[Text(imagery.legends[1]), Container(child: Icon(Icons.info, color: Color(0x00000000)), color: imagery.dec2hex(imagery.getPixelValue(imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[0], imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[1], 1)))]),
+                  Row(children: <Widget>[Text(imagery.legends[2]), Container(child: Icon(Icons.info, color: Color(0x00000000)), color: imagery.dec2hex(imagery.getPixelValue(imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[0], imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[1], 2)))]),
+                  Row(children: <Widget>[Text(imagery.legends[3]), Container(child: Icon(Icons.info, color: Color(0x00000000)), color: imagery.dec2hex(imagery.getPixelValue(imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[0], imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[1], 3)))]),
+                  Row(children: <Widget>[Text(imagery.legends[4]), Container(child: Icon(Icons.info, color: Color(0x00000000)), color: imagery.dec2hex(imagery.getPixelValue(imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[0], imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[1], 4)))]),
+                  Row(children: <Widget>[Text(imagery.legends[5]), Container(child: Icon(Icons.info, color: Color(0x00000000)), color: imagery.dec2hex(imagery.getPixelValue(imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[0], imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[1], 5)))]),
+                  Row(children: <Widget>[Text(imagery.legends[6]), Container(child: Icon(Icons.info, color: Color(0x00000000)), color: imagery.dec2hex(imagery.getPixelValue(imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[0], imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[1], 6)))]),
+                  Row(children: <Widget>[Text(imagery.legends[7]), Container(child: Icon(Icons.info, color: Color(0x00000000)), color: imagery.dec2hex(imagery.getPixelValue(imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[0], imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[1], 7)))]),
+                  Row(children: <Widget>[Text(imagery.legends[8]), Container(child: Icon(Icons.info, color: Color(0x00000000)), color: imagery.dec2hex(imagery.getPixelValue(imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[0], imagery.geoToPixel(testCoord.latitude, testCoord.longitude)[1], 8)))]),
                 ]
               )
             )
@@ -40,10 +50,6 @@ class ForecastScreenState extends State<ForecastScreen> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.refresh),
         onPressed: () async {
-          // for debugging coordinate/pixel conversion
-          //List<int> coord = imagery.coordinateToPixel(LatLng(43.5,-80.5));
-          //print(coord.toString());
-          //print(imagery.decodedForecasts[1].getPixelSafe(coord[0], coord[1]));
           if (await update.remoteImagery(context, false, true)) {
             await update.legends();
             await update.forecasts();
