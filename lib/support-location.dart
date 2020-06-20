49c8bf70012f39366ff07921b984600c0eb6dbf6
Morgan Lim <morgan.lim@mail.mcgill.ca>
2020-06-20 03:07:18 -0400

    save and load location list and associated settings as sharedprefs. separate service/permission requests from location getting logic.

--- lib/support-location.dart
+++ lib/support-location.dart
@@ -1,47 +1,103 @@
 import 'package:location/location.dart';
 import 'package:latlong/latlong.dart';
 
+import 'package:flutter/material.dart';
 import 'package:Nowcasting/main.dart' as main;
 
-// TODO turning off location services causes message spam and crash
-// likely caused by polling from flutter_map plugin
-// TODO set current alert/outage data dynamically
-// TODO save/load places and names on changes/app open
-// TODO adding/removing entries from places/placeNames in forecast interface
-
 Location location = new Location();
-bool _serviceEnabled;
-PermissionStatus _permissionGranted;
-LocationData _locationData;
 
 LatLng lastKnownLocation;
-List<LatLng> places = [lastKnownLocation, LatLng(37.5407, -77.4360)];
-List<String> placeNames = ['Current Location', 'McGill'];
+bool notifyLoc = false;
+List<LatLng> places = [LatLng(45.504688, -73.574990)];
+List<String> placeNames = ['McGill Downtown Campus'];
+List<bool> notify = [false];
 
 bool weatherAlert = false;
-bool radarOutage = true;
-String radarOutageText = 'Some Environment Canada radar sites are currently offline and this may affect update availability. Tap for more info.';
+bool radarOutage = false;
+String radarOutageText = 'The nowcasting service is currently experiencing an outage. This may be due to unscheduled outages in Environment Canada\'s radar system. Tap for more info.';
 String alertText = 'Severe weather alert at your location. Tap for more info.';
 String radarOutageUrl = 'https://www.canada.ca/en/environment-climate-change/services/weather-general-tools-resources/radar-overview/outages-maintenance.html';
-String alertUrl = 'https://google.com/not-set';
+String alertUrl = 'url-not-set';
 
-restorePlaces() async {
-  List<String> _loadPlaces = main.prefs.getStringList('places');
-  List<String> _loadNames = main.prefs.getStringList('placeNames');
-  if (true) {
-    List<double> _loadPlacesDouble = [];
+restorePlaces(BuildContext context) async {
+  print('location.restorePlaces: Starting restore process');
+  List<String> _loadPlaces;
+  List<String> _loadNames;
+  List<String> _loadNotify;
+  try {
+    _loadPlaces = main.prefs.getStringList('places');
+    _loadNames = main.prefs.getStringList('placeNames');
+    _loadNotify = main.prefs.getStringList('notify');
+    notifyLoc = main.prefs.getBool('notifyLoc');
 
-  } else {
-
+  } catch(e) {
+    // If the load failed already because the sharedprefs did not exist yet, just set to defaults
+    print('location.restorePlaces: Arrays did not match in length. Reverting to defaults. Error was '+e.toString());
+    places = [LatLng(45.504688, -73.574990)];
+    placeNames = ['McGill Downtown Campus'];
+    notify = [false];
+    notifyLoc = false;
+    return;
   }
+  List<double> _loadPlacesDouble = [];
+  List<LatLng> _loadPlacesLatLng = [];
+  List<bool> _loadNotifyBool = [];
+  if (_loadPlaces != null && _loadPlaces.isNotEmpty) {
+    for (String _s in _loadPlaces) {
+      _loadPlacesDouble.add(double.parse(_s));
+    }
+    for (int i = 0; i < _loadPlaces.length; i+=2) {
+      _loadPlacesLatLng.add(LatLng(_loadPlacesDouble[i], _loadPlacesDouble[i+1]));
+    }
+    places = _loadPlacesLatLng;
+  } else {
+    places = [];
+  }
+  placeNames = _loadNames;
+  if (_loadNotify != null && _loadNotify.isNotEmpty) {
+    for (String _s in _loadNotify) {
+      _loadNotifyBool.add(bool.fromEnvironment(_s));
+    }
+    notify = _loadNotifyBool;
+  } else {
+    notify = [];
+  }
+  try {
+    assert(places.length == placeNames.length && placeNames.length == notify.length);
+  } catch(e) {
+    // If the load failed somehow based on the above test, reset to defaults
+    print('location.restorePlaces: Arrays did not match in length. Reverting to defaults. Error was '+e.toString());
+    places = [LatLng(45.504688, -73.574990)];
+    placeNames = ['McGill Downtown Campus'];
+    notify = [false];
+    notifyLoc = false;
+    return;
+  }
+  print('location.restorePlaces: Successfully restored locations');
 }
 
 savePlaces() async {
-  for (LatLng place in places) {
-
+  print('update.SavePlaces: Starting save process');
+  List<String> _savePlaces = [];
+  for (LatLng _place in places) {
+    String _lat = _place.latitude.toDouble().toString();
+    String _lon = _place.longitude.toDouble().toString();
+    _savePlaces.add(_lat);
+    _savePlaces.add(_lon);
   }
+  main.prefs.setStringList('places', _savePlaces);
+  main.prefs.setStringList('placeNames', placeNames);
+  List<String> _saveNotify = [];
+  for (bool _n in notify) {
+    _saveNotify.add(_n.toString());
+  }
+  main.prefs.setStringList('notify', _saveNotify);
+  main.prefs.setBool('notifyLoc', notifyLoc);
 }
 
+// Save/restore/update functions for last known location using SharedPreferences.
+// This lets us provide a "current location" sliver and map marker
+// even when the user has subsequently turned off location services.
 restoreLastKnownLocation() async {
   double _loadLat = main.prefs.getDouble('lastKnownLatitude');
   double _loadLon = main.prefs.getDouble('lastKnownLongitude');
@@ -74,20 +130,63 @@ updateLastKnownLocation() async {
   }
 }
 
-getUserLocation() async {
+Future<bool> checkLocService() async {
+  bool _serviceEnabled;
   _serviceEnabled = await location.serviceEnabled();
   if (!_serviceEnabled) {
-    _serviceEnabled = await location.requestService();
-    if (!_serviceEnabled) {
-      print('support-location: Could not get location, serviceEnabled is '+_serviceEnabled.toString());
+    return false;
+  }
+  return true;
+}
+
+Future<bool> checkLocPerm() async {
+  PermissionStatus _permissionGranted;
+  _permissionGranted = await location.hasPermission();
+  if (_permissionGranted != PermissionStatus.granted) {
+    return false;
+  }
+  return true;
+}
+
+requestLocService() async {
+  bool _serviceEnabled;
+  _serviceEnabled = await location.requestService();
+  if (!_serviceEnabled) {
+    print('support-location: Could not enable location service, user rejected prompt');
+    return false;
+  }
+  return true;
+}
+
+requestLocPerm() async {
+  PermissionStatus _permissionGranted;
+  _permissionGranted = await location.requestPermission();
+  if (_permissionGranted != PermissionStatus.granted) {
+    print('support-location: Could not get location permission, user rejected prompt');
+    return false;
+  }
+  return true;
+}
+
+getUserLocation([bool _withRequests = false]) async {
+  LocationData _locationData;
+  if (await checkLocService() == false) {
+    if (_withRequests) {
+      await requestLocService();
+    } else {
+      return;
+    }
+    if (await checkLocService() == false) {
       return;
     }
   }
-  _permissionGranted = await location.hasPermission();
-  if (_permissionGranted == PermissionStatus.denied) {
-    _permissionGranted = await location.requestPermission();
-    if (_permissionGranted != PermissionStatus.granted) {
-      print('support-location: Could not get location, permissionGranted is '+_permissionGranted.toString());
+  if (await checkLocPerm() == false) {
+    if (_withRequests) {
+      await requestLocPerm();
+    } else {
+      return;
+    }
+    if (await checkLocPerm() == false) {
       return;
     }
   }
