a2c07847c39c465d5d46a070f528b8e52102d004
mlim15 <27998345+the-salami@users.noreply.github.com>
2021-01-07 01:16:54 -0500

    add ways to save and load cache

--- lib/support-io.dart
+++ lib/support-io.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:convert';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:Nowcasting/support-imagery.dart' as imagery;
 
 Directory appDocPath;
 
@@ -17,4 +19,25 @@ File localFile(String fileName) {
 String localFilePath(String fileName) {
   String pathName = join(appDocPath.path, fileName);
   return pathName;
+}
+
+// This functionality is only useful if the app is opened within 10 minutes
+// and it has also been killed in the background before the user opens it again,
+// or the user has no internet when opening the second time.
+// Frankly it's of very limited use, but it will certainly improve the experience
+// on very low end devices if the app is frequently opened and closed.
+saveForecastCache(int _index) async {
+  File _file = localFile('forecast.$_index.cache');
+  _file.writeAsStringSync(json.encode(imagery.forecastCache[_index]));
+  print('imagery.saveForecastCache: Finished saving decoded values for image $_index');
+}
+
+loadForecastCaches() async {
+  for (int i = 0; i <= 8; i++) {
+    File _file = localFile('forecast.$i.cache');
+    if (_file.existsSync()) {
+      imagery.forecastCache[i] = json.decode(_file.readAsStringSync());
+    }
+  }
+  print('imagery.loadForecastCache: Finished loading cached image values');
 }
