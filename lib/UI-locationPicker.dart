3188d4094f39039ba3f9db96cda44c42f5e23d8d
mlim15 <27998345+the-salami@users.noreply.github.com>
2021-01-09 03:53:26 -0500

    also swap to FileImage on picker screen

--- lib/UI-locationPicker.dart
+++ lib/UI-locationPicker.dart
@@ -90,7 +90,7 @@ class LocationPickerScreenState extends State<LocationPickerScreen> with Widgets
                     ),
                     opacity: 0.5,
                     imageProvider: io.localFile('forecast.0.png').existsSync() 
-                      ? MemoryImage(io.localFile('forecast.0.png').readAsBytesSync()) 
+                      ? FileImage(io.localFile('forecast.0.png')) 
                       : AssetImage('assets/launcher/logo.png'),
                     gaplessPlayback: true,
                   )
