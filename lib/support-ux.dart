64853ab6aef197bd783ac5c5e14fbbf127750257
mlim15 <27998345+the-salami@users.noreply.github.com>
2021-01-10 17:09:37 -0500

    more general error message for notification onboarding

--- lib/support-ux.dart
+++ lib/support-ux.dart
@@ -21,7 +21,7 @@ final onboardCannotContinueSnack = SnackBar(behavior: SnackBarBehavior.floating,
 final locationOffSnack = SnackBar(behavior: SnackBarBehavior.floating, content: Text('Cannot update location. Check permissions or turn on location services.'));
 final restoreErrorSnack = SnackBar(behavior: SnackBarBehavior.floating, content: Text('Error restoring location list. It has been reset to default.'));
 // Snack bars about notifications
-final notificationPermissionErrorSnack = SnackBar(behavior: SnackBarBehavior.floating, content: Text('To display notifications, you\'ll need to grant the app permission.'),);
+final notificationPermissionErrorSnack = SnackBar(behavior: SnackBarBehavior.floating, content: Text('To display notifications, you\'ll need to grant the requested permissions.'),);
 
 // Theme definitions
 final nowcastingColor = const Color(0xFF0075b3);
